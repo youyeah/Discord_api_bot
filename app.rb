5d3b139c37b3e04a3db27d195a25d7d18580d54e
youyeah <youyeah.k.05@gmail.com>
2019-07-04 10:57:46 +0900

    initial commit

---
+++ app.rb
@@ -0,0 +1,27 @@
+require "discordrb"
+require "dotenv"
+
+CLIEND_ID = ENV['CLIEND_ID']
+TOKEN = ENV['TOKEN']
+class Bot
+    attr_accessor :bot
+
+    def initialize
+        @bot = Discordrb::Commands::CommandBot.new(client_id: 590183973179621416, token: "NTkwMTgzOTczMTc5NjIxNDE2.XRzGuA.W-ARcsPUKZXwLvl-g-sN1FOm9CE", prefix: "?")
+    end
+    
+    def start
+        setting
+
+        @bot.run
+    end
+
+    def setting
+
+        @bot.ready do
+            @bot.game = "起動中"
+        end
+    end
+end
+
+Bot.new.start
